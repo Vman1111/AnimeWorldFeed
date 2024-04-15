0f498da56bc6ca02873af10d09cbdf4d24ad57a3
Vytautas Sapranavicius <visap@protonmail.com>
2024-04-15 11:54:40 +0100

    Initial Commit

---
+++ AnimeFeed/AnimeFeed.h
@@ -0,0 +1,18 @@
+//
+//  AnimeFeed.h
+//  AnimeFeed
+//
+//  Created by Vytautas Sapranavicius on 15/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnimeFeed.
+FOUNDATION_EXPORT double AnimeFeedVersionNumber;
+
+//! Project version string for AnimeFeed.
+FOUNDATION_EXPORT const unsigned char AnimeFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimeFeed/PublicHeader.h>
+
+
