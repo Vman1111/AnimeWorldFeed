85a62d4b771a63a26189bdd6558a1868d168e549
Vytautas Sapranavicius <visap@protonmail.com>
2024-04-16 18:28:28 +0100

    Delivers invalidData error on 200 HTTP response with invalid json.

--- AnimeFeedTests/RemoteAnimeFeedLoaderTests.swift
+++ AnimeFeedTests/RemoteAnimeFeedLoaderTests.swift
@@ -61,6 +61,18 @@ final class RemoteAnimeFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedError = [RemoteAnimeFeedLoader.Error]()
+        sut.load { capturedError.append($0) }
+        
+        let invalidJSON = Data("Invalid JSON".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
     // MARK: Helpers
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (sut: RemoteAnimeFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -83,14 +95,14 @@ final class RemoteAnimeFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success((data, response)))
         }
     }
     
