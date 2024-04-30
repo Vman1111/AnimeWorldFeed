14fda32e2a7cf42d0b6a178bf44a43c05a56e1c7
Vytautas Sapranavicius <visap@protonmail.com>
2024-04-30 18:30:27 +0100

    Moved WEBPImags to own file

--- AnimeFeed/Anime Feed Feature/Models/AnimeItem.swift
+++ AnimeFeed/Anime Feed Feature/Models/AnimeItem.swift
@@ -42,15 +42,3 @@ extension AnimeItem: Decodable {
         case background
     }
 }
-
-public struct WEBPImages: Decodable, Equatable {
-    public let image_url: String
-    public let small_image_url: String
-    public let large_image_url: String
-    
-    public init(image_url: String, small_image_url: String, large_image_url: String) {
-        self.image_url = image_url
-        self.small_image_url = small_image_url
-        self.large_image_url = large_image_url
-    }
-}
