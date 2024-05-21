d274f7563cea880020e79c09df267e517f393e76
Vytautas Sapranavicius <visap@protonmail.com>
2024-05-21 12:35:27 +0100

    Moved mapping logic to a new type `AnimeItemsMapper`

--- AnimeFeed/AnimeFeedAPI/RemoteAnimeFeedLoader.swift
+++ AnimeFeed/AnimeFeedAPI/RemoteAnimeFeedLoader.swift
@@ -27,12 +27,10 @@ public final class RemoteAnimeFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if response.statusCode == 200, let animeRoot = try? JSONDecoder().decode(AnimeRoot.self, from: data) {
-                    let response = AnimeResponse(data: animeRoot.data.map { $0.item },
-                                                 pagination: animeRoot.pagination.item)
-                    completion(.success(response))
-                    
-                } else {
+                do {
+                    let animeRoot = try AnimeItemsMapper.map(data, response)
+                    completion(.success(animeRoot))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -42,36 +40,47 @@ public final class RemoteAnimeFeedLoader {
     }
 }
 
-private struct AnimeRoot: Decodable {
-    let data: [Item]
-    let pagination: PaginationItem
-}
+private class AnimeItemsMapper {
+    private struct AnimeRoot: Decodable {
+        let data: [Item]
+        let pagination: PaginationItem
+    }
 
-private struct Item: Decodable {
-    let mal_id: Int64
-    let url: String?
-    let images: Images
-    let synopsis: String?
-    let background: String?
+    private struct Item: Decodable {
+        let mal_id: Int64
+        let url: String?
+        let images: Images
+        let synopsis: String?
+        let background: String?
+        
+        var item: AnimeItem {
+            return AnimeItem(id: mal_id, url: url, images: images, synopsis: synopsis, background: background)
+        }
+    }
+
+    private struct PaginationItem: Decodable {
+        let last_visible_page: Int?
+        let has_next_page: Bool?
+        let items: PaginationItemCount
+        
+        var item: Pagination {
+            return Pagination(lastVisiblePage: last_visible_page, hasNextPage: has_next_page, count: items.count, total: items.total, perPage: items.per_page)
+        }
+    }
+
+    private struct PaginationItemCount: Decodable {
+        let count: Int?
+        let total: Int?
+        let per_page: Int?
+    }
     
-    var item: AnimeItem {
-        return AnimeItem(id: mal_id, url: url, images: images, synopsis: synopsis, background: background)
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> AnimeResponse {
+        guard response.statusCode == 200 else {
+            throw RemoteAnimeFeedLoader.Error.invalidData
+        }
+        
+        let root = try JSONDecoder().decode(AnimeRoot.self, from: data)
+        return AnimeResponse(data: root.data.map { $0.item },
+                             pagination: root.pagination.item )
     }
 }
-
-private struct PaginationItem: Decodable {
-    let last_visible_page: Int?
-    let has_next_page: Bool?
-    let items: PaginationItemCount
-    
-    var item: Pagination {
-        return Pagination(lastVisiblePage: last_visible_page, hasNextPage: has_next_page, count: items.count, total: items.total, perPage: items.per_page)
-    }
-}
-
-private struct PaginationItemCount: Decodable {
-    let count: Int?
-    let total: Int?
-    let per_page: Int?
-}
-
