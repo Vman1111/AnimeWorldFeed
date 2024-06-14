7ed73b52ad1baf633a7cb9a4487241225c72d344
Vytautas Sapranavicius <visap@protonmail.com>
2024-06-14 20:39:36 +0100

    Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

---
+++ AnimeFeedTests/AnimeFeedAPI/URLSessionHTTPClientTests.swift
@@ -0,0 +1,45 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  AnimeFeedTests
+//
+//  Created by Vytautas Sapranavicius on 14/06/2024.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    private class URLSessionSpy: URLSession, @unchecked Sendable {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {}
+    
+}
