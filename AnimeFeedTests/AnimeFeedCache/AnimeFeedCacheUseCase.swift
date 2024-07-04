91a24ce8e55670d83ae6c9022e6f340385416434
Vytautas Sapranavicius <visap@protonmail.com>
2024-07-04 14:24:53 +0100

    Create `AnimeStoreSpecs` that must be implemented by any `FeedStore` test implementation to guarantee it meets spec

---
+++ AnimeFeedTests/AnimeFeedCache/AnimeFeedCacheUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  AnimeFeedCacheUseCase.swift
+//  AnimeFeedTests
+//
+//  Created by Vytautas Sapranavicius on 04/07/2024.
+//
+
+import XCTest
+
+final class AnimeFeedCacheUseCase: XCTestCase {
+
+    
+
+}
