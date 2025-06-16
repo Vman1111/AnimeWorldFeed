47d5096919f80dd7d60eb944902430fbe6df101a
Vytautas Sapranavicius <visap@protonmail.com>
2025-06-16 15:04:11 +0100

    Save command succeeds on succesful cache insertion

--- AnimeFeedTests/AnimeFeedCache/CacheAnimeFeedUseCaseTests.swift
+++ AnimeFeedTests/AnimeFeedCache/CacheAnimeFeedUseCaseTests.swift
@@ -141,6 +141,25 @@ final class CacheAnimeFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, insertionError)
     }
     
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        let insertionError = anyNSError()
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: (any Error)?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertion(with: insertionError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
